ee8938ccc3617b9238411a59b936f6fd71929396
gbodocares <gbodocares17@gmail.com>
2024-10-03 15:23:03 +0100

    resolved update photo

--- updatePhoto.js
+++ updatePhoto.js
@@ -2,7 +2,7 @@ function changeImage(){
     firebase.auth().onAuthStateChanged((user) => {
         var docRef = db.collection("students").doc(user.uid);
         const ref = firebase.storage().ref();
-        const file = document.getElementById('photo').files[0];
+        const file = document.getElementById('profileImg').files[0];
         const kname = +new Date() + "-" + file.name;
         const metadata = {
             contentType: file.type
