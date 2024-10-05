8e30eb38d26d95521087bed4adf46895cb6d6ebe
gbodocares <gbodocares17@gmail.com>
2024-10-05 21:05:59 +0100

    updated the key

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCwvnxfZXvls5zUzkjpB5I4K0hDRBjz7Ks",
-    authDomain: "gclc-8b187.firebaseapp.com",
-    projectId: "gclc-8b187",
-    databaseURL: "https://gclc-8b187-default-rtdb.firebaseio.com",
-    storageBucket: "gclc-8b187.appspot.com",
-    messagingSenderId: "938242549157",
-    appId: "1:938242549157:web:76c75e475e76b59490f44c"
+    apiKey: "AIzaSyCzeXvcgkgLgQu1Pj2qce4wANvbe7ChWO4",
+    authDomain: "gclc-eabb4.firebaseapp.com",
+    databaseURL: "https://gclc-eabb4-default-rtdb.firebaseio.com",
+    projectId: "gclc-eabb4",
+    storageBucket: "gclc-eabb4.appspot.com",
+    messagingSenderId: "588542989378",
+    appId: "1:588542989378:web:ee83a42419ccaeac0249db"
 };
 
 firebase.initializeApp(firebaseConfig);
