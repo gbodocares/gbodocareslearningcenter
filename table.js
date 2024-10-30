267096248dbf1c14b27c287a6cede73d6f8326f3
gbodocares <gbodocares17@gmail.com>
2024-10-30 16:44:31 +0100

    class rankings

--- table.js
+++ table.js
@@ -6,60 +6,91 @@
 // const sortedArray = sortAsc(array);
 // console.log("Sorted Array (Ascending):", sortedArray);
 
-db.collection("students").where("dept", "==", "ui/ux design")
-    .get()
-    .then((querySnapshot) => {
-    var Users = [];
+
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      
+      var uid = user.uid;
+      
+      var docRef = db.collection("students").doc(uid);
+      
+      docRef.get().then((doc) => {
+            if (doc.exists) {
+                var studentDetails = doc.data();
+                console.log("Document data:", studentDetails);
+                
+
+                db.collection("students").where("dept", "==", studentDetails.dept)
+                .get()
+                .then((querySnapshot) => {
+                    var Users = [];
+               
+                
+                    querySnapshot.forEach((doc) => {
+                        // console.log(doc.data());
+                        let allNames = doc.data().firstname + ' ' + doc.data().surname;
+                        let allScores = doc.data().test + doc.data().attendance + doc.data().softSkillAtt + doc.data().softSkillProject + doc.data().finalProject;
+                        let studentObj = {
+                            personName: allNames,
+                            personScore: allScores
+                        }
+                        Users.push(studentObj);
+                        Users.sort((a, b) => parseFloat(b.personScore) - parseFloat(a.personScore));
+                        console.log(Users)
+                        let count = 1;
+                        var classDataTable = Users.map((data) => {
+                            return `<div>
+                                <tr>
+                                    <td>${count++}</td>
+                                    <td>${data.personName}</td>
+                                    <td>${data.personScore}</td>
+                                </tr>
+                            </div>
+                            `
+                        }).join(' ');
+                        
+                        document.getElementById('classDataTable').innerHTML = classDataTable;
+                    });
+                });
+            
+
+
+
+            
+            }
+        })
+    }
+})
+
+// db.collection("students").where("dept", "==", studentDetails.dept)
+//     .get()
+//     .then((querySnapshot) => {
+//     var Users = [];
    
     
-    querySnapshot.forEach((doc) => {
-        // console.log(doc.data());
-        let allNames = doc.data().firstname + ' ' + doc.data().surname;
-        let allScores = doc.data().test + doc.data().attendance + doc.data().softSkillAtt + doc.data().softSkillProject + doc.data().finalProject;
-        let studentObj = {
-            personName: allNames,
-            personScore: allScores
-        }
-        Users.push(studentObj);
-        Users.sort((a, b) => parseFloat(b.personScore) - parseFloat(a.personScore));
-        console.log(Users)
-        let count = 1;
-        var classDataTable = Users.map((data) => {
-            return `<div>
-                <tr>
-                    <td>${count++}</td>
-                    <td>${data.personName}</td>
-                    <td>${data.personScore}</td>
-                </tr>
-            </div>
-            `
-        }).join(' ');
-        // var count = 1;
-        // var allScores = [];
-        // var classDataTable = Users.map((data) => {
-        // // var studentId = data.studentId;
+//     querySnapshot.forEach((doc) => {
+//         // console.log(doc.data());
+//         let allNames = doc.data().firstname + ' ' + doc.data().surname;
+//         let allScores = doc.data().test + doc.data().attendance + doc.data().softSkillAtt + doc.data().softSkillProject + doc.data().finalProject;
+//         let studentObj = {
+//             personName: allNames,
+//             personScore: allScores
+//         }
+//         Users.push(studentObj);
+//         Users.sort((a, b) => parseFloat(b.personScore) - parseFloat(a.personScore));
+//         console.log(Users)
+//         let count = 1;
+//         var classDataTable = Users.map((data) => {
+//             return `<div>
+//                 <tr>
+//                     <td>${count++}</td>
+//                     <td>${data.personName}</td>
+//                     <td>${data.personScore}</td>
+//                 </tr>
+//             </div>
+//             `
+//         }).join(' ');
         
-        // var studentNames = data.firstname + ' ' + data.surname;
-        // var totalMark = data.attendance + data.test + data.softSkillAtt + data.softSkillProject + data.finalProject;
-        // allScores.push(totalMark);
-        // allScores.sort(function(a, b){return b - a});
-
-        // console.log("all scores" + allScores);
-
-        // // console.log(('alltotal:' + allScores))
-        //     return `<div>
-            
-        //     <tr>
-        //         <td>${count++}</td>
-        //         <td>${studentNames}</td>
-        //         <td>${totalMark}</td>
-        //     </tr>
-            
-        //     </div>
-        //     `
-        // }).join(' ')
-        
-        //cities.push(doc.data().name);
-        document.getElementById('classDataTable').innerHTML = classDataTable;
-    });
-});
+//         document.getElementById('classDataTable').innerHTML = classDataTable;
+//     });
+// });
